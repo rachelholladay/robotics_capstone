aa4fe9f9fc0ed90945a6cf3537697e76f0d50bd6
Neil Jassal <neil.jassal@gmail.com>
2017-02-20 22:01:43 -0500

    Added skeleton for computing locomotion

--- code/subsystems/locomotion.py
+++ code/subsystems/locomotion.py
@@ -20,5 +20,29 @@ class LocomotionSystem(object):
         """
         return self.locomotionData
 
+    def computeMotorCommands(self, start, goal):
+        """
+        Uses the starting and goal DirectedPoints to compute unnormalized
+        and uncalibrated motor commands for each motor.
+        Equations from: https://www.roboteq.com/index.php/component/easyblog/entry/driving-mecanum-wheels-omnidirectional-robots?Itemid=1208
+        @param start DirectedPoint for the robot's current position/orientation
+        @param goal DirectedPoint for the robot's goal position/orientation
+        @return LocalizationData struct containing unnormalized and
+            uncalibrated motor commands
+        """
+        command = LocomotionData()
+
+        # Compute desired angle for the robot to translate at
+        angle = 0
+
+        # Compute robot speed [-1, 1]
+        magnitude = 0
+
+        # Compute rotation speed [0, 2pi]
+        rotation_speed = 0
+
+
+        return command
+
 
 
