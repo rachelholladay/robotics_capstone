32ea5407e5d23551e52c242258173833c0ecf512
Neil Jassal <neil.jassal@gmail.com>
2017-04-28 13:55:46 -0700

    Removed waypoint testing and old loop code

--- code/offboard_controller.py
+++ code/offboard_controller.py
@@ -82,7 +82,7 @@ class OffboardController(object):
         if len(self.badPath.path) is 0:
             self.badPath.path.append(Waypoint(badStart, cst.WRITE_DISABLE))
 
-        self.paths = [self.bluePath, self.badPath]
+        self.paths = [self.bluePath, self.bluePath]
         print("blue path")
         print(self.bluePath)
         print("bad path")
@@ -93,15 +93,7 @@ class OffboardController(object):
         '''
         Main offboard controller loop
         '''
-        print('offboard main loop')
-
-        ######## DEBUG WAYPOINT TESTING ##########
-        wp1 = Waypoint(DirectedPoint(5, 5, 0), cst.WRITE_DISABLE)
-        wp2 = Waypoint(DirectedPoint(5, 8, 0), cst.WRITE_ENABLE)
-        test_target = wp1.target
-        test_write = wp1.write_status
-        debug_waypoint = 0
-        ########## END DEBUG WAYPOINT TESTING #########
+        print('offboard main loop')#
 
         # Setup robot parameters for loop
         self.stop_status = [cst.ROBOT_MOVE, cst.ROBOT_MOVE]
@@ -130,113 +122,12 @@ class OffboardController(object):
             self.sys_comm.sendTCPMessages()
         time.sleep(1)
 
-        
+        # Run actual loop
         while True:
-
-            ########## DEBUG WAYPOINT TESTING ############
-            # # Test to move to 2 waypoints and write when moving to wp2
-            # try:
-            #     # Get localization data
-            #     data = self.sys_localization.getLocalizationData()
-            #     blue_tf = data.robots[cst.TAG_ROBOT1]
-            #     # waypoint testing
-            #     # if at waypoint 1, use waypoint 2
-            #     if debug_waypoint is 0 and blue_tf.dist(wp1.target) < cst.STOP_DIST:
-            #         print("At waypoint 1")  
-            #         print(blue_tf,"| ",blue_tf.dist(wp1.target))
-            #         print(blue_tf,"| ", blue_tf.dist(wp2.target))
-            #         debug_waypoint = 1
-            #         test_write = wp2.write_status
-            #         test_target = wp2.target
-
-            #         # send stop command
-            #         self.sys_comm.generateMessage(
-            #             robot_id=cst.BLUE_ID, locomotion=self.stop_locomotion,
-            #             error=None)
-            #         self.sys_comm.sendTCPMessages()
-            #         time.sleep(1)
-
-            #     if debug_waypoint is 1 and blue_tf.dist(wp2.target) < cst.STOP_DIST:
-            #         print("At waypoint 2, stopping")
-            #         print(blue_tf,"| ",blue_tf.dist(wp1.target))
-            #         print(blue_tf,"| ", blue_tf.dist(wp2.target))
-            #         stop_status = 1
-            #         debug_waypoint = 2
-
-            #     # Theta correction
-            #     test_target.theta = data.corners[cst.TAG_TOP_RIGHT].theta                      
-                
-            #     test_locomotion = LocomotionData(
-            #         tf_robot=blue_tf, 
-            #         tf_target=test_target,
-            #         write_status=cst.WRITE_DISABLE,
-            #         stop_status=stop_status)
-
-            #     self.sys_comm.generateMessage(
-            #         robot_id=cst.BLUE_ID, locomotion=test_locomotion, 
-            #         error=None)
-
-            #     self.sys_comm.sendTCPMessages()
-            #     continue
-
-            # except:
-            #     continue
-            ################# END WAYPOINT WITH WRITING TEST #########
-
             try:
                 data = self.sys_localization.getLocalizationData()
-                self.commandRobot(cst.BLUE_ID, data)
-                # for rid in self.robot_ids:
-                #     self.commandRobot(rid)
-                
-                # # print("=========== new iteration ============")
-                # data = self.sys_localization.getLocalizationData()
-                # blue_tf = data.robots[cst.TAG_ROBOT1]
-                # # print("blue pos: ", str(blue_tf))
-
-                # # If at the waypoint, set next waypoint
-                # if blue_tf.dist(blue_target) < cst.STOP_DIST:
-                #     print("Waypoint", path_index, " reached:", 
-                #             str(self.bluePath[path_index]))
-                #     print("Blue tf: ", str(blue_tf))
-
-                #     # Set next waypoint
-                #     path_index += 1
-
-                #     # Check if at the last waypoint, then stop
-                #     if path_index >= self.bluePath.length:
-                #         stop_status = 1
-                #         print("FINAL WAYPOINT REACHED")
-
-                #     blue_target = self.bluePath[path_index].target
-                #     write_status = self.bluePath[path_index].write_status
-                    
-                #     # send temporary stop command and also actuate writing
-                #     # tool to new position
-                #     stop_wp = self.stop_locomotion
-                #     stop_wp.write_status = write_status
-                #     self.sys_comm.generateMessage(
-                #         robot_id=cst.BLUE_ID, locomotion=stop_wp, 
-                #         error=None)
-                #     self.sys_comm.sendTCPMessages()
-                #     time.sleep(1)
-
-                # # print(str(blue_tf),"| ",blue_tf.dist(blue_target))
-                    
-                # # Theta correction
-                # blue_target.theta = data.corners[cst.TAG_TOP_RIGHT].theta
-
-                # blue_locomotion = LocomotionData(
-                #     tf_robot=blue_tf, 
-                #     tf_target=blue_target,
-                #     write_status=write_status,
-                #     stop_status=stop_status)
-
-                # self.sys_comm.generateMessage(
-                #     robot_id=cst.BLUE_ID, locomotion=blue_locomotion, 
-                #     error=None)
-                # self.sys_comm.sendTCPMessages()
-
+                for rid in self.robot_ids:
+                    self.commandRobot(rid, data)
             except:
                 pass
 
@@ -332,6 +223,6 @@ if __name__ == "__main__":
     oneLine = 'oneLine'
     centerLine = 'centerLine'
 
-    controller = OffboardController(robot_ids=blueID, drawing_name=oneLine)
+    controller = OffboardController(robot_ids=badID, drawing_name=oneLine)
     controller.robotSetup()
     controller.loop()
